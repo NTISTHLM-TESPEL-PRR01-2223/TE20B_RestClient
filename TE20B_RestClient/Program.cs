c8f0c3d3d5e7e94576673af01811366b1462e14a
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2022-11-14 13:22:02 +0100

    Initial commit

---
+++ TE20B_RestClient/Program.cs
@@ -0,0 +1,15 @@
+﻿using RestSharp;
+using System.Text.Json;
+
+RestClient pokeClient = new RestClient("https://pokeapi.co/api/v2/");
+RestRequest request = new RestRequest("pokemon/xftjdtyj");
+RestResponse response = pokeClient.GetAsync(request).Result;
+
+if (response.StatusCode.ToString() == "OK")
+{
+  Pokemon p = JsonSerializer.Deserialize<Pokemon>(response.Content);
+  Console.WriteLine(p.Name);
+}
+
+
+Console.ReadLine();
